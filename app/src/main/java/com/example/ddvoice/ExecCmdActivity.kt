3e10dd2ea7895c072bd9aa55fa075b11399ff4d8
lyn <linxiangyang@lgeek.cn>
2018-09-12 17:57:42 +0800

    微调

---
+++ app/src/main/java/com/example/ddvoice/ExecCmdActivity.kt
@@ -0,0 +1,15 @@
+package com.example.ddvoice
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ExecCmdActivity : AppCompatActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        setContentView(R.layout.activity_exec_cmd)
+        
+        
+    }
+}
