612eadaa088a9b7355d8859c2dbd0da3b016e592
lyn <linxiangyang@lgeek.cn>
2018-09-14 10:22:23 +0800

    微调

--- app/src/main/java/com/example/ddvoice/PreferencesActivity.kt
+++ app/src/main/java/com/example/ddvoice/PreferencesActivity.kt
@@ -1,20 +1,26 @@
 package com.example.ddvoice
 
 import android.os.Bundle
+import android.preference.Preference
 import android.preference.PreferenceActivity
 import android.view.WindowManager
 
 
-class PreferencesActivity : PreferenceActivity() {
+class PreferencesActivity : PreferenceActivity(), Preference.OnPreferenceClickListener {
+    override fun onPreferenceClick(preference: Preference?): Boolean {
+        loadSharedPrefs()
+        return true
+    }
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED)
     
         addPreferencesFromResource(R.layout.preferences)
-    }
-    
-    public override fun onDestroy() {
-        super.onDestroy()
-        loadSharedPrefs()
+        
+        findPreference(SP_HOME_KEY_WAKE).onPreferenceClickListener = this
+        findPreference(SP_VOICE_WAKE).onPreferenceClickListener = this
+        findPreference(SP_VOLUME_KEY_WAKE).onPreferenceClickListener = this
+        findPreference(SP_EXCLUDE_FROM_RECENTS).onPreferenceClickListener = this
     }
 }
