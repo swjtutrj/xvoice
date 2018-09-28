d982e009baa1fd09eb3637f7af7740d3e0db16ec
lyn <linxiangyang@lgeek.cn>
2018-09-28 10:09:44 +0800

    微调

--- app/src/main/java/com/example/ddvoice/action/Actions.kt
+++ app/src/main/java/com/example/ddvoice/action/Actions.kt
@@ -1,9 +1,11 @@
 package com.example.ddvoice.action
 
 import android.accessibilityservice.AccessibilityService
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.PowerManager
 import android.widget.Toast
 import com.example.ddvoice.*
 import com.example.ddvoice.util.*
@@ -79,9 +81,9 @@ fun wxContact() {
         val shortPinYin = PinyinHelper.getShortPinyin(gWxContact)
         
         if (!findTextPYAndClick(gAccessibilityService, pinYin)) {   //search in main UI first
-            findTextAndClick(gAccessibilityService, "搜索")
+            findTextAndClick( "搜索")
             Thread.sleep(750)
-            findFocusAndPaste(gAccessibilityService, shortPinYin)
+            findFocusAndPaste( shortPinYin)
             Thread.sleep(1000)
             if (!findTextPYAndClick(gAccessibilityService, pinYin)) {
                 //if there is content, don't match short pinyin
@@ -96,20 +98,20 @@ fun wxContact() {
         Thread.sleep(750)
         
         if (gWxContact == "滴答清单") {
-            findTextAndClick(gAccessibilityService, "消息")
+            findTextAndClick( "消息")
             Thread.sleep(500)
         }
         
         if (!findEditableAndPaste(gAccessibilityService, gWxContent)) {
             //语音模式
-            findTextAndClick(gAccessibilityService, "切换到键盘")
+            findTextAndClick( "切换到键盘")
             Thread.sleep(500)
             findEditableAndPaste(gAccessibilityService, gWxContent)
         }
         
         if (gWxContact == "滴答清单") {
             Thread.sleep(500)
-            findTextAndClick(gAccessibilityService, "发送")
+            findTextAndClick( "发送")
         }
         //            gWxContact = ""
         
@@ -125,9 +127,9 @@ fun punchOut() {
     try {
         openApp("com.alibaba.android.rimet")
         Thread.sleep(4000)
-        findTextAndClick(gAccessibilityService, "工作")
+        findTextAndClick( "工作")
         Thread.sleep(1000)
-        findTextAndClick(gAccessibilityService, "考勤打卡")
+        findTextAndClick( "考勤打卡")
     
         /*if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
             Thread.sleep(7000)
@@ -135,7 +137,7 @@ fun punchOut() {
             //        mGlobalActionAutomator.setScreenMetrics(mScreenMetrics)
             mGlobalActionAutomator.setService(gAccessibilityService)
             mGlobalActionAutomator.click(555, 1435)
-            //        if (findTextAndClick(gAccessibilityService, "下班打卡")) {
+            //        if (findTextAndClick( "下班打卡")) {
             Thread.sleep(3000)
             performGlobalAction(AccessibilityService.GLOBAL_ACTION_HOME)
             //        }
@@ -155,13 +157,13 @@ fun turnOnUsageAccess() {
         //            Thread.sleep(450)
         for (x in 0..6) {
             Thread.sleep(800)
-            if (findTextAndClick(gAccessibilityService, "小美")) break
+            if (findTextAndClick( "小美")) break
         }
         
         Thread.sleep(1000)
         
         var msg = ""
-        if (findTextAndClick(gAccessibilityService, "允许访问使用记录", true)) {
+        if (findTextAndClick( "允许访问使用记录", true)) {
             msg = "桌面语音唤醒功能需要查看使用情况权限，已为您开启"
             startChecker()
         } else {
@@ -204,4 +206,69 @@ fun prevMusic() {
     val starter = Intent(gApplicationContext, ExecCmdActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
     starter.action = "past"
     gApplicationContext.startActivity(starter)
+}
+
+
+
+
+fun turnOnScreen() {
+    val pm = gApplicationContext?.getSystemService(Context.POWER_SERVICE) as PowerManager?
+    // 获取PowerManager.WakeLock对象,后面的参数|表示同时传入两个值,最后的是LogCat里用的Tag
+    val wl = pm?.newWakeLock(
+            PowerManager.ACQUIRE_CAUSES_WAKEUP or PowerManager.SCREEN_BRIGHT_WAKE_LOCK,
+            "xvoice:mywakelocktag")
+    wl?.acquire(20000) // 点亮屏幕
+    wl?.release() // 释放
+}
+
+fun search(word: String?, useOtherBrowser: Boolean = false, shouldSpeak: Boolean = true) {
+    if (!word.isNullOrEmpty() && word != "谢主隆恩") {
+        val shortWord = if (word!!.length > 10) "以上内容" else word
+        if (shouldSpeak) speak("搜索$shortWord")
+        loadUrl("https://www.baidu.com/s?word=$word", useOtherBrowser)
+    }
+}
+
+fun musicFM() {
+    //    speak("播放豆瓣fm")
+    sayOK()
+    
+    //    if (gAppNamePackageMap.containsValue("com.netease.cloudmusic")) {
+    val starter = Intent()
+    starter.component = ComponentName("com.netease.cloudmusic", "com.netease" + ".cloudmusic.activity.RedirectActivity")
+    starter.action = Intent.ACTION_VIEW
+    starter.data = Uri.parse("orpheus://radio")
+    starter.flags = Intent.FLAG_RECEIVER_FOREGROUND
+    try {
+        gApplicationContext.startActivity(starter.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+        if (gIsPhoneLocked) speak("可能要解锁哦")
+    } catch (e: Exception) {
+        e.printStackTrace()
+        loadUrl("https://douban.fm", true)
+    }
+    //    } else {
+    //        loadUrl("https://douban.fm", true)
+    //    }
+}
+
+fun loadUrl(url: String, useOtherBrowser: Boolean = false) {
+    if (url.contains("douban.fm") || (!gIsPhoneLocked && useOtherBrowser)) {
+        try {
+            val intent: Intent = Intent.parseUri(url,Intent.URI_INTENT_SCHEME)
+            intent.addCategory("android.intent.category.BROWSABLE")
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            gApplicationContext!!.startActivity(intent)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        if (gIsPhoneLocked) speak("需要解锁哦")
+    } else {
+        //        turnOnScreen()
+        
+        gUrlToLoad = url
+        //        Handler().postDelayed({
+        gApplicationContext!!.startActivity(Intent(gApplicationContext, WebViewAct::class
+                .java).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+        //        }, 6500)
+    }
 }
